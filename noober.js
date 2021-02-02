a30306d7ec56b50e6faf4bdbb404b8035de4ea33
Raman Malik <raman.malik@kellogg.northwestern.edu>
2021-02-01 22:25:44 -0600

    stage_no functions

--- noober.js
+++ noober.js
@@ -1,13 +1,30 @@
+
 async function pageLoaded() {
   let response = await fetch('https://kiei451.com/api/rides.json')
   let json = await response.json()
 
   // writes the returned JSON to the console
   console.dir(json)
-  
+  console.log(json)
+  console.log(json.length)
   // 🔥 start here: write code to loop through the rides
-  
+
+  let rides = json
+  for(i=0; i<rides.length; i++) {
+    let ride = rides[i]
+    for(r=0; r<ride.length; r++) { 
+      if (ride.length > 1) { 
+      levelOfService = 'Noober Pool'
+    } else if (ride[0].purpleRequested) { 
+      levelOfService = 'Noober Purple'
+    } else if (ride[0].numberOfPassengers > 3) { 
+      levelOfService = 'Noober XL'
+    } else { 
+      levelOfService = 'Noober X'
+    }
+  }
+  console.log(levelOfService)
+  }
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
-
